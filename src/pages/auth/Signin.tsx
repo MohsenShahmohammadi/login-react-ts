123a79dfff3112988813f06db44a0ea5dd07286b
mohsen_sh5884 <mohsen_sh5884@yahoo.com>
2024-12-06 00:12:44 -0500

    init

---
+++ src/pages/auth/Signin.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+    
+}
+
+const Signin = (props: Props) => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Signin
